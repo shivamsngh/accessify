2635e7b77dea1eb9d539a6d2b94e403ff0a97e0c
Singh <shivam.d.singh@accenture.com>
2018-01-11 16:34:33 +0530

    First Commit

---
+++ inject.js
@@ -0,0 +1,9 @@
+(function() {
+
+	// just place a div at top right
+	var images = document.getElementsByTagName('img');
+	console.log("IMages", images)
+
+	// alert('inserted self... giggity');
+
+})();

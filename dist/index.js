5a3ad6682a5334ddff75f05c67869cc642dd2153
Singh, Shivam D <shivam.d.singh@accenture.com>
2018-01-19 12:28:28 +0530

    removed build

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-	// for the current tab, inject the "inject.js" file & execute it
-	chrome.tabs.executeScript(tab.ib, {
-		file: 'bundle.js'
-	});
-});

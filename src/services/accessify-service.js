714d609ec31ba81d2ff35c5cbab05ff830bb30a3
Singh, Shivam D <shivam.d.singh@accenture.com>
2018-05-25 08:50:03 +0530

    Started secure server

--- src/services/accessify-service.js
+++ src/services/accessify-service.js
@@ -1,12 +1,13 @@
 const API_URL = `http://52.224.178.135/api`;
 const API_URL_TEST = `http://localhost:3000/api`;
+const API_URL_SECURE = `https://accessify.eastus.cloudapp.azure.com:3000/api`;
 
 export default class ApiService {
 
     constructor() { }
 
     getImageDescriptionFileFromServer(object) {
-        return fetch(`${API_URL}/getImageDescription`, {
+        return fetch(`${API_URL_SECURE}/getImageDescription`, {
             method: 'post',
             headers: {
                 "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
